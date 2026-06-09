4c27f1a80a9e6813f4d7f41d51c0701605148770
vibhu <pariseyshubhang@gmail.com>
2026-06-09 16:55:34 +0530

    updated home message

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app=express()
 
 function main (){
     app.get('/',(req,res)=>{
-        res.send('Hello World')
+        res.send('Hello my bubblies')
     })
     app.listen(3000,()=>{
         console.log('Server is running on port 3000');
