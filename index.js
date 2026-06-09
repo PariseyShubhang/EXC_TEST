355057e6d9b7864cadb1f35710b3e7ed8655b343
vibhu <pariseyshubhang@gmail.com>
2026-06-09 18:35:25 +0530

    retest

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app=express()
 
 function main (){
     app.get('/',(req,res)=>{
-        return res.json({'message': 'Hello my bubblies. GITHUB ACTIONS'})
+        return res.json({'message': 'Hello my bubblies. GITHUB ACTIONS retest'})
     })
     app.listen(3000,()=>{
         console.log('Server is running on port 3000');
