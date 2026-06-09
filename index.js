5ca8d7ee3f257ad8e9c8c945a8e44286d78e4b6d
vibhu <pariseyshubhang@gmail.com>
2026-06-09 16:31:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app=express()
+
+function main (){
+    app.get('/',(req,res)=>{
+        res.send('Hello World')
+    })
+    app.listen(3000,()=>{
+        console.log('Server is running on port 3000');
+    })
+}
+main()
+
